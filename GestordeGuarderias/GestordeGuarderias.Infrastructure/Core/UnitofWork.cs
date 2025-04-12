b0bba5559bdf7883c01975c98a5ef55a9ca7499f
angerypr <angerypr16@gmail.com>
2025-04-12 11:27:51 -0400

    Creando Unit Of Work

---
+++ GestordeGuarderias/GestordeGuarderias.Infrastructure/Core/UnitofWork.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Threading.Tasks;
+
+namespace GestordeGuarderias.Infrastructure.Core
+{
+    public class UnitOfWork : IDisposable
+    {
+        private readonly GestordeGuarderiasDbContext _context;
+        private IDbContextTransaction? _transaction;
+
+        public UnitOfWork(GestordeGuarderiasDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> CompleteAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task BeginTransactionAsync()
+        {
+            _transaction = await _context.Database.BeginTransactionAsync();
+        }
+
+        public async Task CommitTransactionAsync()
+        {
+            if (_transaction != null)
+            {
+                await _transaction.CommitAsync();
+            }
+        }
+
+        public async Task RollbackTransactionAsync()
+        {
+            if (_transaction != null)
+            {
+                await _transaction.RollbackAsync();
+            }
+        }
+
+        public void Dispose()
+        {
+            _transaction?.Dispose();
+            _context.Dispose();
+        }
+    }
+}
